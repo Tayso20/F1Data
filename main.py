56fa2f1757f97930266cf8d6b9ae4ed6e4a74cc2
Taylen <t.houslip@gmail.com>
2023-09-23 19:31:41 +1000

    working to get all points and driver id!

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import requests
+race_Num= 0 
 
-api_url = "http://ergast.com/api/f1/2023/1/results.json"
-test = requests.get(api_url)
-data = test.json()
 test_dic = {}
-race_results = data["MRData"]['RaceTable']['Races'][0]['Results']
-for result in race_results:
-    testA =result['Driver']['driverId']
-    testB =result['points']
 
-    #print(result['Driver']['driverId'],result['points'])
+for race_Num in range(0, 16):
+        api_url = f"http://ergast.com/api/f1/2023/{race_Num}/results.json"
+        test = requests.get(api_url)
+        data = test.json()
+        race_results = data["MRData"]['RaceTable']['Races'][0]['Results']
 
-    test_dic[testA] = testB
+        for result in race_results:
+                testA =result['Driver']['driverId']
+                testB =result['points']
+                #test_dic[testA] = testB
+                print(testA)
+                print(testB)
 
-print(test_dic)
-print("uwu")
+                
+#for result in race_results:
+    #testA =result['Driver']['driverId']
+    #testB =result['points']
+    #test_dic[testA] = testB
+
+#print(test_dic)

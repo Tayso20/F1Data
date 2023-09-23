0acf892c9956de920f393534b1b97569f70c3570
Tayso20 <t.houslip@gmail.com>
2023-09-23 20:28:36 +1000

    i got my data to savce to a json file!

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
 import requests
+import json
 race_Num= 0 
 
-test_dic = {}
+
+allRaces = {}
 
 for race_Num in range(0, 16):
         api_url = f"http://ergast.com/api/f1/2023/{race_Num}/results.json"
         test = requests.get(api_url)
         data = test.json()
         race_results = data["MRData"]['RaceTable']['Races'][0]['Results']
-
+        singlerace = {}
         for result in race_results:
-                testA =result['Driver']['driverId']
-                testB =result['points']
-                #test_dic[testA] = testB
-                print(testA)
-                print(testB)
+                driver =result['Driver']['driverId']
+                pts =result['points']
+                singlerace[driver] = pts
+                allRaces[f"Race{race_Num}"] = singlerace
+ 
+
+
+with open('pleasefuckingwork.json', 'w') as f:
+    json.dump(allRaces, f)
+
+
 
                 
+
+
+
+
 #for result in race_results:
     #testA =result['Driver']['driverId']
     #testB =result['points']
     #test_dic[testA] = testB
-
-#print(test_dic)

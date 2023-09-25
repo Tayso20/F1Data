052285030cc1e898a68300c93b9d5bb64e796968
Tayso20 <t.houslip@gmail.com>
2023-09-25 19:13:17 +1000

    yes

--- main.py
+++ main.py
@@ -6,17 +6,18 @@ race_Num= 1
 allRaces = {}
 
 for race_Num in range(1, 16):
-        api_url = f"http://ergast.com/api/f1/2023/{race_Num}/results.json"
-        test = requests.get(api_url)
-        data = test.json()
-        race_results = data["MRData"]['RaceTable']['Races'][0]['Results']
-        singlerace = {}
-        for result in race_results:
-                driver =result['Driver']['driverId']
-                driver2 = f"Driver:  {driver}"
-                pts =result['points']
-                singlerace[driver2] = pts
-                allRaces[f"Race{race_Num}"] = singlerace
+    api_url = f"http://ergast.com/api/f1/2023/{race_Num}/results.json"
+    test = requests.get(api_url)
+    data = test.json()
+    race_results = data["MRData"]['RaceTable']['Races'][0]['Results']
+    singlerace = {}
+
+    for result in race_results:
+        driver =result['Driver']['driverId']
+        driver2 = f"Driver:  {driver}"
+        pts =result['points']
+        singlerace[driver2] = pts
+        allRaces[f"Race{race_Num}"] = singlerace
  
 
 
